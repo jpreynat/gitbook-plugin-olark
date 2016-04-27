74d29c1e43bc534d568ad07d3b9b2a3064de3a7b
Johan Preynat <johan.preynat@gmail.com>
2016-04-27 13:15:44 +0200

    Remove unused file

--- book/plugin.js
+++
@@ -1,9 +0,0 @@
-require(["gitbook"], function(gitbook) {
-    gitbook.events.bind("start", function(e, config) {
-        config.site_id = config.site_id || {};
-    });
-
-    gitbook.events.bind("exercise.submit", function(e, data) {
-
-    });
-});
